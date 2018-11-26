fbc84f14f9cad5804785b40a07dee468db93b3bb
youssufAmr <youssufAmr>
2018-11-26 18:06:03 +0200

    Initial commit

---
+++ src/tryrepo/Functions.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tryrepo;
+
+/**
+ *
+ * @author ESCA
+ */
+public class Functions {
+    public static void isEven(int num){}
+    public static void isOdd(int num){}
+    public static void isPrime(int num){}
+}
