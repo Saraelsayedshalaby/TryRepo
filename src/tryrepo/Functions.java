49bc32b45c007f2291b194c53d45407d34780f03
Saraelsayedshalaby <45360299+Saraelsayedshalaby@users.noreply.github.com>
2018-11-26 19:43:36 +0200

    ADD new function
    add new function to check if number is even or not

--- src/tryrepo/Functions.java
+++ src/tryrepo/Functions.java
@@ -10,7 +10,28 @@ package tryrepo;
  * @author ESCA
  */
 public class Functions {
-    public static void isEven(int num){}
+    public static void isEven(int num){
+    
+    
+    int num;
+    System.out.println("Enter an Integer number:");
+
+    //The input provided by user is stored in num
+    Scanner input = new Scanner(System.in);
+    num = input.nextInt();
+
+    /* If number is divisible by 2 then it's an even number
+     * else odd number*/
+    if ( num % 2 == 0 )
+        System.out.println("Entered number is even");
+     else
+        System.out.println("Entered number is odd");
+
+    
+    
+    
+    
+    }
     public static void isOdd(int num){}
     public static void isPrime(int num){}
 }
