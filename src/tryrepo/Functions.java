36c321ad14aadd49c48a241169c4c372703915d9
youssufAmr <youssufAmr>
2018-11-26 20:00:01 +0200

    add function isPrime

--- src/tryrepo/Functions.java
+++ src/tryrepo/Functions.java
@@ -11,27 +11,24 @@ package tryrepo;
  */
 public class Functions {
     public static void isEven(int num){
-    
-    
-    int num;
-    System.out.println("Enter an Integer number:");
-
-    //The input provided by user is stored in num
-    Scanner input = new Scanner(System.in);
-    num = input.nextInt();
-
     /* If number is divisible by 2 then it's an even number
      * else odd number*/
     if ( num % 2 == 0 )
         System.out.println("Entered number is even");
      else
         System.out.println("Entered number is odd");
-
-    
-    
-    
-    
     }
     public static void isOdd(int num){}
-    public static void isPrime(int num){}
+    public static void isPrime(int num){
+        int numberofdevisors=0;
+        for(int i=1;i<num;++i){
+            if(num%i==0)
+                numberofdevisors++;
+        }
+        if(numberofdevisors>2 || num==1)
+            System.out.println("its not prime");
+        else
+           System.out.println("its prime"); 
+            
+    }
 }
